8b33840f700d87bf8718eb162e46f67b4c50a2d0
Alhxe <alexhh1999@gmail.com>
2022-05-15 15:40:41 +0200

    Update ConfigManager.java

--- internal/src/main/java/di/internal/controller/file/ConfigManager.java
+++ internal/src/main/java/di/internal/controller/file/ConfigManager.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Map;
 import java.util.Optional;
+import java.util.logging.Level;
 
 import org.yaml.snakeyaml.Yaml;
 
@@ -20,10 +21,13 @@ public class ConfigManager implements FileController {
 	private File customConfigFile;
 
 	private Map<String, Object> yamlData;
+	
+	private PluginController pluginController;
 
 	@SuppressWarnings("unchecked")
 	public ConfigManager(PluginController controller, File dataFolder) {
 		this.customConfigFile = new File(dataFolder, FILENAME);
+		this.pluginController = controller;
 		if (!this.customConfigFile.exists()) {
 			this.customConfigFile.getParentFile().mkdirs();
 			saveResource(controller.getPlugin(), dataFolder, FILENAME, false);
@@ -38,30 +42,43 @@ public class ConfigManager implements FileController {
 
 	public String getString(String path) {
 		char specialChar = (char) 167;
-		return this.yamlData.get(path).toString().replace('&', specialChar);
+		return get(path).replace('&', specialChar);
 	}
 
 	public long getLong(String path) {
-		return Long.parseLong(this.yamlData.get(path).toString());
+		return Long.parseLong(get(path));
 	}
 
 	public boolean getBoolean(String path) {
-		return Boolean.parseBoolean(this.yamlData.get(path).toString());
+		return Boolean.parseBoolean(get(path));
 	}
 
 	public int getInt(String path) {
-		return Integer.parseInt(this.yamlData.get(path).toString());
+		return Integer.parseInt(get(path));
 	}
 
 	public Optional<Long> getOptionalLong(String path) {
 		try {
-			return Optional.ofNullable(Long.parseLong(this.yamlData.get(path).toString()));
+			return Optional.ofNullable(Long.parseLong(get(path)));
 		} catch (NumberFormatException e) {
 			return Optional.empty();
 		}
 	}
-	
+
 	public boolean contains(String path) {
 		return this.yamlData.containsKey(path);
+	}
+
+	private String get(String path) {
+		try {
+			return this.yamlData.get(path).toString();
+		} catch (Exception e) {
+			String message = "Failed to find in config file: "+path;
+			String message2 = "Regenerate the file to correct the problem or include the above mentioned line in the configuration file.";
+			pluginController.getPlugin().getLogger().log(Level.SEVERE, message);
+			pluginController.getPlugin().getLogger().log(Level.SEVERE, message2);
+			pluginController.getPlugin().getServer().getPluginManager().disablePlugin(pluginController.getPlugin());
+		}
+		return "error";
 	}
 }
